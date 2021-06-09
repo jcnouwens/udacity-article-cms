90176e0356400c3d00c40d358725e7b908c8cd53
jesse.nouwens <jcnouwens@gmail.com>
2021-06-09 10:18:06 +0200

    feat: env LOGLEVEL

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,7 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.logger.setLevel(logging.DEBUG)
+app.logger.setLevel(logging.os.environ.get("LOGLEVEL"))
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
